cb5ef7d154a9b597d35bf41bf075e870e6944d8a
Jason Lam <66794251+jasonlam912@users.noreply.github.com>
2023-04-14 17:17:12 +0800

    del all java file
    del all java file

--- app/src/main/java/com/jasonstudio/cookbook2/Network/CallbackListener.java
+++
@@ -1,8 +0,0 @@
-package com.jasonstudio.cookbook2.Network;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public interface CallbackListener {
-    void getResult(JSONObject jsonObject) throws JSONException;
-}
